106bb68ff882155fedb7e16039c8b2b74c21204a
Kenbaz <kbassey016@gmail.com>
2025-12-21 13:57:25 +0100

    index file for providers exports

---
+++ src/components/providers/index.ts
@@ -0,0 +1,13 @@
+export { QueryProvider } from "./QueryProvider";
+
+export {
+  ToastProvider,
+  showSuccessToast,
+  showErrorToast,
+  showInfoToast,
+  showWarningToast,
+  showLoadingToast,
+  updateToast,
+  dismissToast,
+  toastPromise,
+} from "./ToastProvider";
