4044f9e94561205d106a7cbdea97aa7796dbeb0a
Kenbaz <kbassey016@gmail.com>
2025-12-21 13:56:41 +0100

    Implemented react query provider

---
+++ src/components/providers/QueryProvider.tsx
@@ -0,0 +1,28 @@
+// src/components/providers/QueryProvider.tsx
+'use client';
+
+import { useState } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createQueryClient } from "@/lib/api/queryClient";
+
+
+interface QueryProviderProps { 
+    children: React.ReactNode;
+};
+
+
+export function QueryProvider({ children }: QueryProviderProps) {
+    // Initialize QueryClient only once
+    const [queryClient] = useState(() => createQueryClient());
+
+    return (
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
+      </QueryClientProvider>
+    );
+};
