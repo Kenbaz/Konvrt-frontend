7d2485f41957fdcc02e017d8b91c8aa21e3dd7f9
Kenbaz <kbassey016@gmail.com>
2025-12-22 01:41:43 +0100

    index file for exporting upload components

---
+++ src/components/uploadsUI/index.ts
@@ -0,0 +1,5 @@
+export { FileUploadZone, SimpleDropZone } from "./FileUploadZone";
+export type { FileUploadZoneProps } from "./FileUploadZone";
+
+export { FilePreview, FileInfo } from "./Filepreview";
+export type { FilePreviewProps } from "./Filepreview";
