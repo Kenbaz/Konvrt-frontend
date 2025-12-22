83efca5592426cb114bcbd0943dc2836ab779e5a
Kenbaz <kbassey016@gmail.com>
2025-12-22 03:25:12 +0100

    updated index file for uploadsUi components exports

--- src/components/uploadsUI/index.ts
+++ src/components/uploadsUI/index.ts
@@ -3,3 +3,10 @@ export type { FileUploadZoneProps } from "./FileUploadZone";
 
 export { FilePreview, FileInfo } from "./Filepreview";
 export type { FilePreviewProps } from "./Filepreview";
+
+export {
+  UploadProgress,
+  UploadProgressCompact,
+  UploadProgressBar,
+} from "./UploadProgress";
+export type { UploadProgressProps } from "./UploadProgress";
