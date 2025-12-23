1a3cf7943fe3e48f3b852cb3b0935653f266ff0b
Kenbaz <kbassey016@gmail.com>
2025-12-23 18:00:48 +0100

    updated jobs hook index file

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -6,3 +6,53 @@ export type {
   UseUploadOptions,
   UploadStatus,
 } from "./useUpload";
+
+// Job creation hooks
+export {
+  useCreateJob,
+  useCreateJobMutation,
+  type CreateJobInput,
+  type CreateJobState,
+  type CreateJobResult,
+  type UseCreateJobOptions,
+  type UseCreateJobReturn,
+} from "./useCreateJob";
+
+// Operation hooks
+export {
+  useOperations,
+  useGroupedOperations,
+  useOperationsByMediaType,
+  useOperationDetail,
+  usePrefetchOperations,
+  useOperationFromCache,
+  useInvalidateOperations,
+  hasOperations,
+  hasGroupedOperations,
+} from "./useOperations";
+
+export {
+  useJobs,
+  useRecentJobs,
+  useJobsByStatus,
+  useActiveJobsCount,
+  prefetchJobs,
+  type UseJobsOptions,
+  type UseJobsReturn,
+  type UseRecentJobsOptions,
+  type UseRecentJobsReturn,
+  type UseJobsByStatusOptions,
+} from "./useJobs";
+
+export {
+  useJobPolling,
+  useMultiJobPolling,
+  useJobWithPolling,
+  useIsJobActive,
+  type UseJobPollingOptions,
+  type UseJobPollingReturn,
+  type UseMultiJobPollingOptions,
+  type MultiJobPollingState,
+  type UseJobWithPollingOptions,
+  type UseJobWithPollingReturn,
+} from "./useJobPolling";
